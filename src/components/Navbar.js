acd552426612fc24a6ed2dd9aacfd93b5774864e
anthonykimani <anthonykimani@student.moringaschool.com>
2022-09-05 17:26:41 +0300

    Adds basic routes

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <div className="flex ">
+            <h2>Safari Travels</h2>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+            </ul>
+            <button>Register</button>
+        </div>
+     );
+}
+ 
+export default Navbar;
