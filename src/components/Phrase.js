0baf1606de99b417d88ae53656b77c70c1db0927
anthonykimani <anthonykimani@student.moringaschool.com>
2022-09-11 13:31:32 +0300

    fixes view heights

--- src/components/Phrase.js
+++ src/components/Phrase.js
@@ -1,5 +1,5 @@
 const Phrase = () => {
-  return <div className="mt-[20em] flex flex-col text-center justify-around p-6 xsm:mt-[25em] sm:mt-[25em] md:mt-[25em] lg:mt-[38em]">
+  return <div className="mt-[40em] flex flex-col text-center justify-around p-6 xsm:mt-[45em] sm:mt-[45em] md:mt-[45em] lg:mt-[45em]">
       <h1 className="text-5xl font-bold py-5">Explore the True spirit of Africa</h1>
       <div className="flex flex-col justify-around py-5">
           <h3 className="w-54">Over <span className="text-blue">200+</span> Locations </h3>
